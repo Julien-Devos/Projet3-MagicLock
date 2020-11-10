357c6cf370b9bb9eb70c2eafd95dd3e0e4a23048
Julien-Devos <julien.devos@student.uclouvain.be>
2020-11-10 01:27:34 +0100

    updated MagicLock.py

--- MagicLock.py
+++ MagicLock.py
@@ -1,6 +1,22 @@
-print ("hello julien")
+def main():
+    # Le nom du fichier de QCM à utiliser
+    filename = 'QCM2.txt'
 
-def function(a,b,c,d):
-    print(a,b,c,d)
+    try:
+        # Load questions from file
+        questions = load_questions(filename)
 
-function(3,2,2,4)
+        # User choose the score mode
+        scores = select_rating_mode()
+
+        print("Bienvenu(e) dans notre programme de QCM !"
+              "\n\tPour répondre aux questions, rentrez la lettre associée aux propositions de réponses."
+              "\n\tSi vous pensez qu'il y a plusieurs réponses, écrivez les en séparant chaque réponse par une virgule !\n"
+              "\n********************************\n")
+
+        qcm_loop(questions, scores)
+
+        print("\nLe questionnaire est terminé !")
+
+    except OSError:
+        print("Il y a un problème avec le fichier de questionnaire !")
