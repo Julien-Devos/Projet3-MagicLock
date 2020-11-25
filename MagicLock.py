531cb3fe14b32a1d08cb8aff573c9e23aba1a03d
Julien-Devos <julien.devos@student.uclouvain.be>
2020-11-25 01:42:28 +0100

    finished mission 10

--- MagicLock.py
+++ MagicLock.py
@@ -1,3 +1,17 @@
 #Projet n°3, groupe A04:
 #Béranger Deketelaere, Bryan Devos, Romain Gobert, Julien Devos
 print("yo")
+def going_tocrash():
+    pass
+
+def dont():
+    pass
+
+def no_crash_tesla():
+    if going_tocrash():
+        dont()
+
+
+
+
+
