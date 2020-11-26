d2ac73008aaf4f9d93e9bc50776af31a9f62f7db
Julien-Devos <julien.devos@student.uclouvain.be>
2020-11-26 17:44:12 +0100

    modified MagicLock.py and added message and code

--- MagicLock.py
+++ MagicLock.py
@@ -1,3 +1,82 @@
 from sense_hat import SenseHat
+import time
+
+s = SenseHat()
+s.low_light = True
+
+G = (0, 255, 0)
+Y = (255, 255, 0)
+B = (0, 0, 255)
+R = (255, 0, 0)
+W = (255,255,255)
+P = (255,105, 180)
+O = (0,0,0)
+
+screen_off = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+correct = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+
+wrong = [
+    R, O, O, O, O, O, O, R,
+    O, R, O, O, O, O, R, O,
+    O, O, R, O, O, R, O, O,
+    O, O, O, R, R, O, O, O,
+    O, O, O, R, R, O, O, O,
+    O, O, R, O, O, R, O, O,
+    O, R, O, O, O, O, R, O,
+    R, O, O, O, O, O, O, R,
+    ]
+
+def pin():
+    correct_pin = [("pressed", "up"), ("released", "up"), ("pressed", "down"), ("released", "down"),
+                   ("pressed", "left"),
+                   ("released", "left"), ("pressed", "right"), ("released", "right")]
+    pin_correct = False
+    current_pin = []
+
+    while not pin_correct:
+        s.set_pixels(screen_off)
+        event = s.stick.wait_for_event()
+        current_pin.append((event.action, event.direction))
+
+        if current_pin == correct_pin:
+            pin_correct = True
+
+        elif len(current_pin) == len(correct_pin):
+            current_pin = []
+            # TODO mettre un affichage de leds
+            print("Wrong pin !")
+            s.set_pixels(wrong)
+            time.sleep(1)
+
+    # TODO mettre un affichage de leds
+    print("Pin correct")
+    time.sleep(1)
+
+def check_msg():
+
+
+while True:
+    pin() #demande le code pin à l'utilisateur
+
+
 
 
