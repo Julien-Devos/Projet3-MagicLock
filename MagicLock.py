e0f2d5f17f115ff55c739c7686fb72e00cdf27d9
Julien-Devos <julien.devos@student.uclouvain.be>
2020-11-26 19:25:16 +0100

    end of first session (26/11) display options and pin

--- MagicLock.py
+++ MagicLock.py
@@ -4,6 +4,10 @@ import time
 s = SenseHat()
 s.low_light = True
 
+state = {
+    "menu_index" : 0
+    }
+
 G = (0, 255, 0)
 Y = (255, 255, 0)
 B = (0, 0, 255)
@@ -24,25 +28,36 @@ screen_off = [
     ]
 
 correct = [
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, G,
+    O, O, O, O, O, O, G, G,
+    O, O, O, O, O, G, G, G,
+    G, O, O, O, G, G, G, O,
+    G, G, O, G, G, G, O, O,
+    G, G, G, G, G, O, O, O,
+    O, G, G, G, O, O, O, O,
+    O, O, G, O, O, O, O, O,
     ]
 
-wrong = [
-    R, O, O, O, O, O, O, R,
-    O, R, O, O, O, O, R, O,
-    O, O, R, O, O, R, O, O,
-    O, O, O, R, R, O, O, O,
-    O, O, O, R, R, O, O, O,
-    O, O, R, O, O, R, O, O,
-    O, R, O, O, O, O, R, O,
-    R, O, O, O, O, O, O, R,
+cancel = [
+    R, R, O, O, O, O, R, R,
+    R, R, R, O, O, R, R, R,
+    O, R, R, R, R, R, R, O,
+    O, O, R, R, R, R, O, O,
+    O, O, R, R, R, R, O, O,
+    O, R, R, R, R, R, R, O,
+    R, R, R, O, O, R, R, R,
+    R, R, O, O, O, O, R, R,
+    ]
+
+save = [
+    O, O, G, G, G, G, O, O,
+    O, G, G, G, G, G, G, O,
+    O, G, G, O, O, O, O, O,
+    O, G, G, G, G, O, O, O,
+    O, O, G, G, G, G, O, O,
+    O, O, O, O, O, G, G, O,
+    O, G, G, G, G, G, G, O,
+    O, O, G, G, G, G, O, O,
     ]
 
 def pin():
@@ -59,24 +74,81 @@ def pin():
 
         if current_pin == correct_pin:
             pin_correct = True
+            print("Pin correct")
+            s.set_pixels(correct)
+            time.sleep(1)
 
         elif len(current_pin) == len(correct_pin):
             current_pin = []
-            # TODO mettre un affichage de leds
             print("Wrong pin !")
-            s.set_pixels(wrong)
+            s.set_pixels(cancel)
             time.sleep(1)
 
     # TODO mettre un affichage de leds
-    print("Pin correct")
-    time.sleep(1)
 
 def check_msg():
+    with open("message", 'r') as message:
+        if message.readlines() == ["\n"]:
+            return True
+        else:
+            return False
+
+def display_choice(menu):
+    choice = menu[state["menu_index"] % len(menu)]
+
+    s.set_pixels(choice)
+    """if choice == save:
+        s.set_pixels(save)
+
+    elif choice == cancel:
+        s.set_pixels(cancel)"""
+
+
+def choosed_option(menu):
+    choice = menu[state["menu_index"] % len(menu)]
+
+    print("Vous avez choisis l'option n°" + str(state["menu_index"] % len(menu) + 1))
+
+def show_menu(menu):
+    choosed = False
+
+    s.set_pixels(menu[0])
+    while not choosed:
+        events = s.stick.get_events()
+        if events:
+            for event in events:
+                if event.action != "pressed":
+                    continue
+
+                if event.direction == 'left':
+                    state["menu_index"] += 1
+                    display_choice(menu)
+
+                elif event.direction == 'right':
+                    state["menu_index"] -= 1
+                    display_choice(menu)
+
+                elif event.direction == 'middle':
+                    choosed = True
+                    choosed_option(menu)
 
 
 while True:
-    pin() #demande le code pin à l'utilisateur
+    # demande le code pin à l'utilisateur
+    pin()
+
+    if check_msg():
+        """ Demander d'enregistrer un message """
+        print("vide")
+
+        menu_options = [save,cancel]
+
+        show_menu(menu_options)
 
 
 
+    else:
+        """ propose les options du message """
+        print("pas vide")
+        raise SystemExit
 
