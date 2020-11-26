74c059f0cf2847e4d4c8e20ffd2c1df0d765aabb
Julien-Devos <julien.devos@student.uclouvain.be>
2020-11-26 16:37:48 +0100

    modified MagicLock.py and added sense_hat.py

--- MagicLock.py
+++ MagicLock.py
@@ -1,17 +1,3 @@
-#Projet n°3, groupe A04:
-#Béranger Deketelaere, Bryan Devos, Romain Gobert, Julien Devos
-print("yo")
-def going_tocrash():
-    pass
-
-def dont():
-    pass
-
-def no_crash_tesla():
-    if going_tocrash():
-        dont()
-
-
-
+from sense_hat import SenseHat
 
 
